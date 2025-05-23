52547bef81fe5d89593f28b0b97b32cb35e5e0d9
Riceball LEE <snowyu.lee@gmail.com>
2025-05-23 14:47:50 +0800

    refactor: init only pass  ../../build-wasm

--- wasm/test_page/start_server.sh
+++ wasm/test_page/start_server.sh
@@ -9,31 +9,29 @@ Usage: $(basename "$0") [ARTIFACTS_SOURCE_FOLDER]
 
 SCRIPT_ABSOLUTE_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-if [ "$#" -ne 1 ]; then
-    echo "Illegal number of parameters passed"
-    echo "$usage"
-    exit
-fi
+# if [ "$#" -ne 1 ]; then
+#     echo "Illegal number of parameters passed"
+#     echo "$usage"
+#     exit
+# fi
 
 # Check if ARTIFACTS_SOURCE_FOLDER is valid or not
-if [ ! -e "$1" ]; then
-    echo "Error: Folder \""$1"\" doesn't exist"
-    exit
+# start_server.sh ../../build-wasm
+if [ -e "$1" ]; then
+    # Prepare a list all wasm artifacts to be copied and copy them to the destination folder
+    ARTIFACTS_BASE_NAME="bergamot-translator-worker"
+    ARTIFACTS="$1/$ARTIFACTS_BASE_NAME.js $1/$ARTIFACTS_BASE_NAME.wasm"
+    ARTIFACTS_DESTINATION_FOLDER=$SCRIPT_ABSOLUTE_PATH/../module/worker
+
+    for i in $ARTIFACTS; do
+        [ -f "$i" ] || breaks
+        cp $i $ARTIFACTS_DESTINATION_FOLDER
+        echo "Copied \"$i\" to \"$ARTIFACTS_DESTINATION_FOLDER\""
+    done
 fi
-
-# Prepare a list all wasm artifacts to be copied and copy them to the destination folder
-ARTIFACTS_BASE_NAME="bergamot-translator-worker"
-ARTIFACTS="$1/$ARTIFACTS_BASE_NAME.js $1/$ARTIFACTS_BASE_NAME.wasm"
-ARTIFACTS_DESTINATION_FOLDER=$SCRIPT_ABSOLUTE_PATH/../module/worker
-
-for i in $ARTIFACTS; do
-    [ -f "$i" ] || breaks
-    cp $i $ARTIFACTS_DESTINATION_FOLDER
-    echo "Copied \"$i\" to \"$ARTIFACTS_DESTINATION_FOLDER\""
-done
 
 # Start http server
 (cd $SCRIPT_ABSOLUTE_PATH;
 npm install;
 echo "Start httpserver";
-node bergamot-httpserver.js 80 1 0)
+node bergamot-httpserver.js 8080 1 0)
